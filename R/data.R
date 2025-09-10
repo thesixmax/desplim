c9dd0d5d878e3bf8ccc848f6d8cffe6b2064812f
Sixten Maximillian Thestrup <sixten.thestrup@gmail.com>
2025-09-10 02:24:46 +0200

    feat: add sf example data

--- R/data.R
+++ R/data.R
@@ -1,10 +1,10 @@
 #' DESPLIM compactness data
 #'
 #' Clean data used to model the DESPLIM compactness metric, based on
-#' the data provided by Kaufmann et al (2021). The metrics have been computed
+#' the data provided by Kaufmann et al. (2021). The metrics have been computed
 #' using the `redistmetrics` package.
 #'
-#' @format ## `desplim_compactness_data`
+#' @format ## `desplim_compact_data`
 #' A data frame with 558 rows and 11 columns:
 #' \describe{
 #'   \item{compact}{District compactness}
@@ -19,8 +19,23 @@
 #'   \item{sym_x}{X symmetry compactness}
 #'   \item{sym_y}{Y symmetry compactness}
 #' }
-#' 
+#'
 #' @source <https://dataverse.harvard.edu/file.xhtml?fileId=4143644&version=1.1>
 #' @source <https://github.com/aaronrkaufman/compactness/tree/e91519e15fd3ca55f57fc21323bb12ed5a66305b/data>
 #' @source <https://alarm-redist.org/redistmetrics/>
-"desplim_compactness_data"
+"desplim_compact_data"
+
+#' DESPLIM sf data
+#'
+#' Example districts with compactness score from the data provided by 
+#' Kaufmann et al. (2021).
+#'
+#' @format ## `desplim_sf_data`
+#' A sf data frame with 25 rows and 2 columns:
+#' \describe{
+#'   \item{compact}{District compactness}
+#'   \item{geometry}{District shape}
+#' }
+#'
+#' @source <https://dataverse.harvard.edu/file.xhtml?fileId=4143644&version=1.1>
+"desplim_sf_data"
